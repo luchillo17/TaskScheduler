adb8d4befe1f7d3386cff3b5a4b2d67ef10fb746
Carlos Esteban Lopez <luchillo17@gmail.com>
2017-06-22 09:03:18 -0500

    Refactor api validators

--- src/app/task/api-task/api.validators.ts
+++ src/app/task/api-task/api.validators.ts
@@ -1,12 +1,10 @@
-import { AbstractControl, FormGroup } from "@angular/forms";
+import { AbstractControl, FormGroup } from '@angular/forms';
 
 export class ApiValidators {
 
-  constructor() { }
-
-  static requestDataByMethod(group: FormGroup): { [key: string]: any} {
-    let requestControl = group.controls['requestData'] as AbstractControl;
-    let methodControl  = group.controls['method'] as AbstractControl;
+  public static requestDataByMethod(group: FormGroup): { [key: string]: any} {
+    const requestControl = group.controls['requestData'] as AbstractControl;
+    const methodControl  = group.controls['method'] as AbstractControl;
 
     if (
       methodControl.value !== 'GET' &&
@@ -17,7 +15,7 @@ export class ApiValidators {
     ) {
       requestControl.setErrors({ required: true })
     } else if (requestControl.errors !== null) {
-      let {required, ...errors} = requestControl.errors;
+      const {required, ...errors} = requestControl.errors;
       // let errors = Object.assign({}, requestControl.errors)
       requestControl.setErrors(errors)
     }
@@ -25,13 +23,13 @@ export class ApiValidators {
     return null
   }
 
-  static validateJson(control: AbstractControl) {
+  public static validateJson(control: AbstractControl) {
     if (control.value === '') {
       return null
     }
 
     try {
-      let o: Object = JSON.parse(control.value)
+      const o: object = JSON.parse(control.value)
       if (o !== undefined && typeof o === 'object') {
         return null;
       }

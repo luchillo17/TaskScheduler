b9ca1015af3f1990fbd307ef3d143741e48cb5ac
Carlos Esteban Lopez <luchillo17@gmail.com>
2017-07-05 17:39:16 -0500

    Drag&Drop background indicator of active drag item

--- src/app/drag-n-drop/draggable.directive.ts
+++ src/app/drag-n-drop/draggable.directive.ts
@@ -13,6 +13,9 @@ export class DraggableDirective {
     return true
   }
 
+  @HostBinding('style.backgroundColor')
+  private backgroundColor
+
   @Input()
   set ngDraggable(data: DraggableData) {
     if (data) {
@@ -23,9 +26,16 @@ export class DraggableDirective {
   @HostListener('dragstart', ['$event'])
   private onDragStart(event: DragEvent) {
 
+    this.backgroundColor = '#5bc0de'
+
     const {data} = this.draggableData
 
     event.dataTransfer.setData('Text', JSON.stringify(data))
+  }
+
+  @HostListener('dragend', ['$event'])
+  private onDragEnd(event: DragEvent) {
+    this.backgroundColor = undefined
   }
 
   private draggableData: DraggableData = {}
